ff4c90314e71bb4cfe88813ed5fce595cdc7c912
Leandro <LeandroSB2003@gmail.com>
2026-05-22 15:11:36 -0500

    Avance 5

---
+++ vite-project/src/Fondo/FondoFinal.tsx
@@ -0,0 +1,10 @@
+import "./Fondo.css";
+import Imagen from "../assets/FondoFinal.png";
+
+
+const Fondo = () => {
+  return <img src={Imagen} alt="Fondo" className="imagenFondo" />;
+
+};
+
+export default Fondo;
--- vite-project/src/routers/Rutas.tsx
+++ vite-project/src/routers/Rutas.tsx
@@ -1,19 +1,15 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import WelcomeView from "../features/game/views/PantallaInicio";
-<<<<<<< HEAD
 import GameView from "../features/game/views/PantallaJuego";
-=======
+import ResultView from "../features/game/views/PantallaResultado";
 import LoginView from "../features/game/views/auth/login";
->>>>>>> c00fd65daba245c13bda1b9809bf42f7095f5a20
 
 
 const router = createBrowserRouter([
   { path: "/", element: <WelcomeView /> },
-<<<<<<< HEAD
   { path: "/game", element: <GameView /> },
-=======
+  { path: "/result", element: <ResultView /> },
   { path: "/login", element: <LoginView /> },
->>>>>>> c00fd65daba245c13bda1b9809bf42f7095f5a20
 ]);
 
 export const AppRouter = () => <RouterProvider router={router} />;
